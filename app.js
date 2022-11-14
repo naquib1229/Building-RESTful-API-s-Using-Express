96a180565827ec8011a16ec58f9d647c270b63c4
naquib1229 <naquib.alam1229@gmail.com>
2022-11-14 08:01:39 +0530

    Introducing express

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-//Express.js or simply Express, is a back end web application framework for building RESTful APIs with Node.js,
-// released as free and open-source software under the MIT License. It is designed for building web applications and APIs. 
-//It has been called the de facto standard server framework for Node.js.
-
-//RESTful services or RESTful APIs
-//REST stands for Representational State Transfer
-//REST is convention to build these http services.
-// Create
-// Read
-// Update
-// Delete
-//We refer to these operation altogether as CRUD Operation.
-//GET /api/customers                read multiple customers
-//GET /api/customers/1              read single customer with specified id
-//PUT /api/customers/1              Update customer
-//DELETE /api/customers/1           Delete customer
-//POST /api/customers               Create customer 
+//Earlier we have seen that by using http module we can make a web server by using http.createServer(req,res)
+//but difficulty arises when there is multiple route to serve
+//for keeping our application code maintainable, we use express framework.
+//mkdir express-demo
+//cd express-demo
+//npm init --yes
+//npm i express
