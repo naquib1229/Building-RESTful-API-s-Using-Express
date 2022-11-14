d0102cb8a2362a32bd5e52a4d03fd729d8e5c4c7
naquib1229 <naquib.alam1229@gmail.com>
2022-11-14 10:14:49 +0530

    Nodemon

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,21 +1,21 @@
-//Building your First Web server.
+//Nodemon
 
-const express = require('express'); //return function
-const app = express(); //function called return object by convention we call it app
+const express = require('express'); 
+const app = express(); 
 
-//app.get();
-//app.post();
-//app.put();
-//app.delete();
-//all these methods correspond with http verbs
 
-app.get('/', (req,res)=>   //first argument url/endpoint here '/' represents root and 2nd callback function with two arguments req, res
+
+app.get('/', (req,res)=>  
                  {
-                    res.send('Hello world');
+                    res.send('Hello world !!!');
                  });
                  
 app.get('/api/courses', (req,res) => {
     res.send([1,2,3]);
 });
 app.listen(3000, () => console.log('Listening on port 3000 ...'));
+
+//npm i -g nodemon
+//Nodemon is a utility depended on about 3 million projects, that will monitor for any changes in your source and automatically restart your server.
+//nodemon indes.js
 
