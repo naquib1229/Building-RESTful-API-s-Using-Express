dfbb69f6ec5f461bdfc8265d99d7c9c2e12eed29
naquib1229 <naquib.alam1229@gmail.com>
2022-11-14 07:46:57 +0530

    Introduction and RESTful services

---
+++ app.js
@@ -0,0 +1,17 @@
+//Express.js or simply Express, is a back end web application framework for building RESTful APIs with Node.js,
+// released as free and open-source software under the MIT License. It is designed for building web applications and APIs. 
+//It has been called the de facto standard server framework for Node.js.
+
+//RESTful services or RESTful APIs
+//REST stands for Representational State Transfer
+//REST is convention to build these http services.
+// Create
+// Read
+// Update
+// Delete
+//We refer to these operation altogether as CRUD Operation.
+//GET /api/customers                read multiple customers
+//GET /api/customers/1              read single customer with specified id
+//PUT /api/customers/1              Update customer
+//DELETE /api/customers/1           Delete customer
+//POST /api/customers               Create customer 
