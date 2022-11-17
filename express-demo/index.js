b85b43b2101eaed3891da0b3381afc6d9ac828f2
naquib1229 <naquib.alam1229@gmail.com>
2022-11-17 08:25:20 +0530

    Creating custom Middleware in separate module

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,16 +1,13 @@
-// Creating custom Middleware 
+// Creating custom Middleware in separate module
 
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express'); 
-const func = require('joi/lib/types/func');
 const app = express(); 
 
 app.use(express.json()); 
 
-app.use(function(req, res, next) {
-    console.log('Logging...');
-    next();
-});
+app.use(logger);
 
 app.use(function(req, res, next) {
     console.log('Authenticating...');
