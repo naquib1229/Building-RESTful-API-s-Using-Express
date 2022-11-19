41e37168fbf01ed60244316b60544dcf53a80d66
naquib1229 <naquib.alam1229@gmail.com>
2022-11-19 22:08:49 +0530

    Structuring Express Application

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,23 +1,16 @@
-// templating Engines
-//Examples- pug, Mustache, EJS
-//A template engine enables you to use static template files in your application. 
-//At runtime, the template engine replaces variables in a template file with actual values,
-// and transforms the template into an HTML file sent to the client. This approach makes it easier to design an HTML page.
-// npm i pug
-// in ./views folder created index.pug
-//render
+// Structuring Express Application
+//
 
 
+const debug = require('debug')('app:startup');
 
-
-
-const startupDebugger = require('debug')('app:startup');
-const dbDebugger = require('debug')('app:db');
 const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
-const logger = require('./logger');
+const logger = require('./middleware/logger');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 const express = require('express'); 
 const app = express(); 
 
@@ -25,6 +18,7 @@ app.set('view engine', 'pug'); //express internally load pug no need of require
 app.set('views', './views'); //optional and default ./views is the location where all views are stored
 
 app.use(helmet());
+app.use('/api/courses', courses);
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -36,82 +30,9 @@ console.log('Mail Password  ' + config.get('mail.password') );
 
 if(app.get('env') === 'development') {
   app.use(morgan('tiny'));  
-  startupDebugger('Morgan enabled....');
+  debug('Morgan enabled....');
 }
-
-//DB work..
-dbDebugger('connected to the database....');
-
-app.use(logger);
-
-const courses = [
-    {id:1, name:'course1'},
-    {id:2, name:'course2'},
-    {id:3, name:'course3'}
-];
-
-app.get('/', (req,res)=>  
-    {
-    res.render('index', {title:'My Express App', message: 'Hello'});
-    });
-                 
-app.get('/api/courses', (req,res) => {
-    res.send(courses);
-}); 
-
-app.post('/api/courses', (req,res) => {
-    
-
-    const { error } = validateCourse(req.body); //object destructuring equivalent to result.error
-    if(error) return  res.status(400).send(error.details[0].message);
-    
-    const course = {
-        id: courses.length + 1 ,
-        name: req.body.name
-    };
-    courses.push(course);
-    
-    res.send(course);
-});
-app.put('/api/courses/:id', (req, res) => {
-    
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with given ID was not found');
-
-    const { error } = validateCourse(req.body); //object destructuring equivalent to result.error
-    if(error) return  res.status(400).send(error.details[0].message);
-        
-    //Update course
-    // Return the updated course
-    course.name = req.body.name;
-    res.send(course);
-
-});
-
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with given ID was not found');
-    res.send(course);
-}); 
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with given ID was not found');
-     
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    res.send(course);
-});
-
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    return Joi.validate(course, schema)
-}
+               
 
 
 //PORTS
---
+++ express-demo/routes/home.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+app.get('/', (req,res)=>  
+{
+res.render('index', {title:'My Express App', message: 'Hello'});
+});
+
+module.exports = router;
